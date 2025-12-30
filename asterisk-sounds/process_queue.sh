92503bfd5e04b31e7ecf81238fe868dbe651c1cd
Jesse Tayler <jtayler@oeinc.com>
2025-12-29 19:35:58 -0500

    first pass of looping sounds...finally - what a fool! ugh.

--- asterisk-sounds/process_queue.sh
+++ asterisk-sounds/process_queue.sh
@@ -6,12 +6,10 @@ SOUNDS_DIR="/var/lib/asterisk/sounds/en"
 
 # Check if the queue file exists and is not empty
 if [[ ! -s $QUEUE_FILE ]]; then
-  echo "No files to process."
-  exit 1
+  # Return an empty string if there are no files in the queue
+  echo "done"
+  exit 0
 fi
-
-# Get the list of current files in the queue
-queue_files=$(cat $QUEUE_FILE)
 
 # Get the next file in the queue (last line) and remove any leading/trailing whitespace
 NEXT_FILE=$(tail -n 1 $QUEUE_FILE | tr -d '[:space:]')
@@ -21,12 +19,9 @@ if [[ -n "$NEXT_FILE" ]]; then
   # Remove the last line from the queue file
   sed -i '$d' $QUEUE_FILE
   
-  # Log the file being processed
-  echo "Processing file: $NEXT_FILE"
-  
   # Return the file name for Asterisk's playback
   echo "$NEXT_FILE"
 else
-  echo "No more files in the queue."
-  exit 1
+  # If no file was found, return an empty string
+  echo "done"
 fi
